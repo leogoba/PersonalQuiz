ced74fcfcb9ec466354af5f9d04f4be61cfe9490
leogoba <leogob>
2022-10-14 22:31:23 +0400

    Final Version

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -30,7 +30,6 @@ class QuestionsViewController: UIViewController {
         }
     }
     
-    
     private let questions = Question.getQuestions()
     private var answersChosen: [Answer] = []
     private var questionIndex = 0
@@ -42,7 +41,12 @@ class QuestionsViewController: UIViewController {
         super.viewDidLoad()
         updateUI()
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answerChosen = answersChosen
+    }
+    
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
         guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
         let currentAnswer = currentAnswers[buttonIndex]
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,14 +9,46 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet var resultLabels: [UILabel]!
+    
+    var answerChosen: [Answer] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        logic()
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
+    }
+}
+
+//MARK: - Private methods - Получаем итоговый результат
+extension ResultViewController {
+    private func logic() {
+        var dogCount = 0
+        var catCount = 0
+        var turtleCount = 0
+        var rabbitCount = 0
+        
+        for answer in answerChosen {
+            switch answer.animal {
+            case .dog:
+                dogCount += 1
+                resultLabels.first?.text = "Вы - \(Animal.dog.rawValue)"
+                resultLabels.last?.text = "\(Animal.dog.definition)"
+            case .cat: catCount += 1
+                resultLabels.first?.text = "Вы - \(Animal.cat.rawValue)"
+                resultLabels.last?.text = "\(Animal.cat.definition)"
+            case .turtle: turtleCount += 1
+                resultLabels.first?.text = "Вы - \(Animal.turtle.rawValue)"
+                resultLabels.last?.text = "\(Animal.turtle.definition)"
+            default: rabbitCount += 1
+                resultLabels.first?.text = "Вы - \(Animal.rabbit.rawValue)"
+                resultLabels.last?.text = "\(Animal.rabbit.definition)"
+            }
+        }
     }
 }
 
